2c173ddf4070448f2091f731161d1870ff1e4142
razi <rezaie127@yahoo.com>
2022-05-05 11:26:01 +0430

    edit  add formula fragment

--- app/src/main/java/com/example/tooskawood/AddFormulaFragment.kt
+++ app/src/main/java/com/example/tooskawood/AddFormulaFragment.kt
@@ -5,24 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tooskawood.databinding.FragmentAddFormulaBinding
 
-
-
-class GetFormulaFragment : Fragment() {
-
+class AddFormulaFragment : Fragment() {
+    lateinit var binding : FragmentAddFormulaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentAddFormulaBinding.inflate (inflater, container, false)
+        return binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_fulmula, container, false)
+        //return inflater.inflate(R.layout.fragment_add_formula, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
