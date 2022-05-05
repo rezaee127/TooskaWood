b5b5e534d17c0347941e5e0d985ef4dc8a10eb26
razi <rezaie127@yahoo.com>
2022-05-05 18:43:35 +0430

    edit ShowFormulaFragment

--- app/src/main/java/com/example/tooskawood/ui/AddFormulaFragment.kt
+++ app/src/main/java/com/example/tooskawood/ui/AddFormulaFragment.kt
@@ -28,7 +28,7 @@ class AddFormulaFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentAddFormulaBinding.inflate (inflater, container, false)
         return binding.root
 
