2162097cad079252579c2d68a34081cf38e1cc83
razi <rezaie127@yahoo.com>
2022-05-05 14:56:15 +0430

    edit ShowFormulaFragment

--- app/src/main/java/ui/ShowFormulaFragment.kt
+++ app/src/main/java/ui/ShowFormulaFragment.kt
@@ -1,16 +1,24 @@
 package ui
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.tooskawood.R
 import com.example.tooskawood.databinding.FragmentShowFormulaBinding
+import viewModel.MainViewModel
 
 
 class ShowFormulaFragment : Fragment() {
     lateinit var binding : FragmentShowFormulaBinding
-
+    val vModel: MainViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,6 +36,57 @@ class ShowFormulaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+        setRecyclerView()
 
+    }
+
+    private fun setRecyclerView() {
+        val formulaAdapter = FormulaAdapter({ goToDetailFragment(it) })
+        binding.formulaRecyclerView.adapter = formulaAdapter
+        formulaAdapter.submitList(vModel.getFormulaList())
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun initView() {
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_showFormulaFragment_to_addFormulaFragment)
+        }
+
+        vModel.getCountFormulaLiveData().observe(requireActivity()) {
+            if (it == 0) {
+                binding.buttonSearch.isEnabled = false
+                binding.editTextSearch.isEnabled = false
+            }
+        }
+
+        search()
+
+    }
+
+    private fun search() {
+
+        binding.buttonSearch.setOnClickListener {
+            if (binding.editTextSearch.text.isNullOrBlank())
+                binding.editTextSearch.error = "یک کد وارد کنید"
+            else if (vModel.searchFormula(binding.editTextSearch.text.toString()) == 0) {
+                val dialog = AlertDialog.Builder(requireContext())
+                dialog.setMessage("The desired code does not exist")
+                    .setPositiveButton("OK",
+                        DialogInterface.OnClickListener { dialog, id ->
+                        })
+                dialog.create().show()
+            } else if (vModel.searchFormula(binding.editTextSearch.text.toString()) != 0) {
+                val id = vModel.searchFormula(binding.editTextSearch.text.toString())
+                goToDetailFragment(id)
+            }
+        }
+    }
+
+    private fun goToDetailFragment(id: Int) {
+        val bundle = bundleOf("id" to id)
+       // findNavController().navigate(R.id.action_showFormulaFragment_to_detailFragment, bundle)
     }
 }
