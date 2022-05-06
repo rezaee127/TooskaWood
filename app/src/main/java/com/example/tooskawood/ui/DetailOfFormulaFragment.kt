72b9796504de0362d7b9dd821348e32987997e86
razi <rezaie127@yahoo.com>
2022-05-06 11:20:58 +0430

    add delete formula

--- app/src/main/java/com/example/tooskawood/ui/DetailOfFormulaFragment.kt
+++ app/src/main/java/com/example/tooskawood/ui/DetailOfFormulaFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.tooskawood.R
 import com.example.tooskawood.databinding.FragmentDetailOfFormulaBinding
 import com.example.tooskawood.viewModel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class DetailOfFormulaFragment : Fragment() {
     lateinit var binding:FragmentDetailOfFormulaBinding
@@ -41,10 +43,36 @@ class DetailOfFormulaFragment : Fragment() {
         binding.textViewFormulaCodeInDetailFragment.text=vModel.getFormula(id).code
         binding.MaterialRecyclerView.adapter=MaterialAdapter(vModel.getFormula(id).materials,id,{edit(id)})
 
+
         binding.buttonEdit.setOnClickListener {
             edit(id)
         }
+
+        binding.buttonDelete.setOnClickListener {
+            delete(id)
+        }
+
     }
+
+    private fun delete(id:Int){
+        val snkbr = Snackbar.make(binding.detailOfFormulaFragment, "Do you want to delete the formula?",
+            Snackbar.LENGTH_LONG)
+
+        snkbr.duration = 9000
+
+        snkbr.setAction("yes",  View.OnClickListener {
+            vModel.deleteById(id)
+            Toast.makeText(requireContext(),"حذف فرمول انجام شد", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_detailOfFormulaFragment_to_showFormulaFragment)
+            snkbr.dismiss()
+        })
+
+            .setTextColor(resources.getColor(R.color.whiteBack2))
+            .setActionTextColor(resources.getColor(R.color.white))
+            .setBackgroundTint(resources.getColor(R.color.gray))
+            .show()
+    }
+
 
     private fun edit(id:Int){
         val bundle = bundleOf("id" to id,"edit" to true)
